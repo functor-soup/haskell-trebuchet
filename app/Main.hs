452e6defecebd86714568945754ae2bb97854c42
luke <nerorevenge@gmail.com>
2017-06-14 22:54:38 -0400

    added stats function

--- app/Main.hs
+++ app/Main.hs
@@ -3,32 +3,11 @@
 module Main where
 
 import Control.Concurrent.Async
-import Data.ByteString as B
-import Data.Semigroup ((<>))
-import Network.Simple.TCP
-import Options.Applicative
-import System.IO.Error
-
+import qualified Data.ByteString as B
 import Lib
-
-data Params = Params
-  { host :: String
-  , port :: String
-  , times :: Int
-  }
-
-sample :: Parser Params
-sample =
-  Params <$> strOption (long "host" <> help "url to overwhelm with requests") <*>
-  strOption (long "port" <> short 'q' <> help "port number") <*>
-  option auto (long "number" <> short 'n' <> help "number of requests to send")
-
-programCli :: ParserInfo Params
-programCli =
-  info
-    (sample <**> helper)
-    (fullDesc <> progDesc "server stress tester" <>
-     header "haskell-trebuchet - a dead simple stress tester of servers")
+import Network.Simple.TCP
+import Options.Applicative (execParser)
+import System.IO.Error
 
 flood :: String -> String -> IO ()
 flood host port =
@@ -38,12 +17,14 @@ flood host port =
        Just y -> B.putStrLn y
        _ -> B.putStrLn "Nothing received")
 
-floodWithException :: String -> String -> IO ()
+floodWithException :: String -> String -> IO Bool
 floodWithException host port =
-  catchIOError (flood host port) (Prelude.putStrLn . ("Exception > " ++) . show)
+  catchIOError
+    (flood host port >> return True)
+    (\x -> (Prelude.putStrLn . ("Exception > " ++) . show $ x) >> return False)
 
-parallelExec :: String -> String -> Int -> IO ()
-parallelExec host port times = mapConcurrently_ (\_ -> floodWithException host port) [0 .. times]
+parallelExec :: String -> String -> Int -> IO [Bool]
+parallelExec host port times = mapConcurrently (\_ -> floodWithException host port) [0 .. times]
 
 main :: IO ()
-main = execParser programCli >>= \a -> parallelExec (host a) (port a) (times a)
+main = execParser programCli >>= \a -> parallelExec (host a) (port a) (times a) >>= putStrLn . stats
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,41 @@
 module Lib
-    ( someFunc
-    ) where
+  ( Params(..)
+  , sample
+  , programCli
+  , stats
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.List as D
+import Data.Semigroup ((<>))
+import Options.Applicative
+
+data Params = Params
+  { host :: String
+  , port :: String
+  , times :: Int
+  }
+
+sample :: Parser Params
+sample =
+  Params <$> strOption (long "host" <> help "url to overwhelm with requests") <*>
+  strOption (long "port" <> short 'q' <> help "port number") <*>
+  option auto (long "number" <> short 'n' <> help "number of requests to send")
+
+programCli :: ParserInfo Params
+programCli =
+  info
+    (sample <**> helper)
+    (fullDesc <> progDesc "server stress tester" <>
+     header "haskell-trebuchet - a dead simple stress tester of servers")
+
+stats :: [Bool] -> String
+stats x =
+  let (t, f) = D.partition id x
+      [lt, lf] = map (fromIntegral . length) [t, f]
+      len = lt + lf
+      tl = "Total number of requests \t\t = " ++ (show $ len)
+      l1 = "Number of failed requests \t\t = " ++ (show $ lf)
+      l1p = "% of failed requests \t\t\t = " ++ (show $ lf / len)
+      l2 = "Number of non-failed requests \t\t = " ++ (show $ lt)
+      l2p = "% of non-failed requests \t\t = " ++ (show $ lt / len)
+  in D.intercalate "\n" [tl, l1, l1p, l2, l2p]
